747c97e3c4bbb3aa042bad61c7129939e653a053
manalsal <menalglt@gmail.com>
2023-08-12 00:37:26 -0700

    python-import_modules

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+
+    sum = 0
+    for a in range(len(sys.argv) - 1):
+        sum += int(sys.argv[a + 1])
+    print("{}".format(sum))
